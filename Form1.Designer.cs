123b1b514a51b6f99fce4387d6b90cea6a53fe68
omar2512 <113863685+omar2512@users.noreply.github.com>
2022-12-14 20:38:11 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Entitylabdbfirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxid = new System.Windows.Forms.TextBox();
+            this.textBoxempname = new System.Windows.Forms.TextBox();
+            this.textBoxdepid = new System.Windows.Forms.TextBox();
+            this.textBoxdepartmentanme = new System.Windows.Forms.TextBox();
+            this.btnempadd = new System.Windows.Forms.Button();
+            this.btnempupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnempdisplay = new System.Windows.Forms.Button();
+            this.btndepadd = new System.Windows.Forms.Button();
+            this.btndepupdate = new System.Windows.Forms.Button();
+            this.btndepdelete = new System.Windows.Forms.Button();
+            this.btndepdisplay = new System.Windows.Forms.Button();
+            this.comboBoxemployee = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.comboBoxemployee);
+            this.groupBox1.Controls.Add(this.btnempdisplay);
+            this.groupBox1.Controls.Add(this.btndelete);
+            this.groupBox1.Controls.Add(this.btnempupdate);
+            this.groupBox1.Controls.Add(this.btnempadd);
+            this.groupBox1.Controls.Add(this.textBoxempname);
+            this.groupBox1.Controls.Add(this.textBoxid);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(653, 304);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "employee";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.btndepadd);
+            this.groupBox2.Controls.Add(this.btndepupdate);
+            this.groupBox2.Controls.Add(this.btndepdelete);
+            this.groupBox2.Controls.Add(this.btndepdisplay);
+            this.groupBox2.Controls.Add(this.textBoxdepid);
+            this.groupBox2.Controls.Add(this.textBoxdepartmentanme);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 322);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(653, 304);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "empoye";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(757, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(282, 596);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "department";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4aid";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "department";
+            // 
+            // textBoxid
+            // 
+            this.textBoxid.Location = new System.Drawing.Point(103, 89);
+            this.textBoxid.Name = "textBoxid";
+            this.textBoxid.Size = new System.Drawing.Size(278, 22);
+            this.textBoxid.TabIndex = 3;
+            // 
+            // textBoxempname
+            // 
+            this.textBoxempname.Location = new System.Drawing.Point(103, 141);
+            this.textBoxempname.Name = "textBoxempname";
+            this.textBoxempname.Size = new System.Drawing.Size(278, 22);
+            this.textBoxempname.TabIndex = 4;
+            // 
+            // textBoxdepid
+            // 
+            this.textBoxdepid.Location = new System.Drawing.Point(123, 122);
+            this.textBoxdepid.Name = "textBoxdepid";
+            this.textBoxdepid.Size = new System.Drawing.Size(278, 22);
+            this.textBoxdepid.TabIndex = 6;
+            // 
+            // textBoxdepartmentanme
+            // 
+            this.textBoxdepartmentanme.Location = new System.Drawing.Point(123, 161);
+            this.textBoxdepartmentanme.Name = "textBoxdepartmentanme";
+            this.textBoxdepartmentanme.Size = new System.Drawing.Size(278, 22);
+            this.textBoxdepartmentanme.TabIndex = 7;
+            // 
+            // btnempadd
+            // 
+            this.btnempadd.Location = new System.Drawing.Point(63, 245);
+            this.btnempadd.Name = "btnempadd";
+            this.btnempadd.Size = new System.Drawing.Size(75, 23);
+            this.btnempadd.TabIndex = 6;
+            this.btnempadd.Text = "add";
+            this.btnempadd.UseVisualStyleBackColor = true;
+            this.btnempadd.Click += new System.EventHandler(this.btnempadd_Click);
+            // 
+            // btnempupdate
+            // 
+            this.btnempupdate.Location = new System.Drawing.Point(234, 245);
+            this.btnempupdate.Name = "btnempupdate";
+            this.btnempupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnempupdate.TabIndex = 7;
+            this.btnempupdate.Text = "update";
+            this.btnempupdate.UseVisualStyleBackColor = true;
+            this.btnempupdate.Click += new System.EventHandler(this.btnempupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(382, 245);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 8;
+            this.btndelete.Text = "delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnempdisplay
+            // 
+            this.btnempdisplay.Location = new System.Drawing.Point(548, 245);
+            this.btnempdisplay.Name = "btnempdisplay";
+            this.btnempdisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnempdisplay.TabIndex = 9;
+            this.btnempdisplay.Text = "diplay";
+            this.btnempdisplay.UseVisualStyleBackColor = true;
+            this.btnempdisplay.Click += new System.EventHandler(this.btnempdisplay_Click);
+            // 
+            // btndepadd
+            // 
+            this.btndepadd.Location = new System.Drawing.Point(63, 261);
+            this.btndepadd.Name = "btndepadd";
+            this.btndepadd.Size = new System.Drawing.Size(75, 23);
+            this.btndepadd.TabIndex = 10;
+            this.btndepadd.Text = "add";
+            this.btndepadd.UseVisualStyleBackColor = true;
+            this.btndepadd.Click += new System.EventHandler(this.btndepadd_Click);
+            // 
+            // btndepupdate
+            // 
+            this.btndepupdate.Location = new System.Drawing.Point(234, 261);
+            this.btndepupdate.Name = "btndepupdate";
+            this.btndepupdate.Size = new System.Drawing.Size(75, 23);
+            this.btndepupdate.TabIndex = 11;
+            this.btndepupdate.Text = "update";
+            this.btndepupdate.UseVisualStyleBackColor = true;
+            this.btndepupdate.Click += new System.EventHandler(this.btndepupdate_Click);
+            // 
+            // btndepdelete
+            // 
+            this.btndepdelete.Location = new System.Drawing.Point(382, 261);
+            this.btndepdelete.Name = "btndepdelete";
+            this.btndepdelete.Size = new System.Drawing.Size(75, 23);
+            this.btndepdelete.TabIndex = 12;
+            this.btndepdelete.Text = "delete";
+            this.btndepdelete.UseVisualStyleBackColor = true;
+            this.btndepdelete.Click += new System.EventHandler(this.btndepdelete_Click);
+            // 
+            // btndepdisplay
+            // 
+            this.btndepdisplay.Location = new System.Drawing.Point(536, 261);
+            this.btndepdisplay.Name = "btndepdisplay";
+            this.btndepdisplay.Size = new System.Drawing.Size(75, 23);
+            this.btndepdisplay.TabIndex = 13;
+            this.btndepdisplay.Text = "diplay";
+            this.btndepdisplay.UseVisualStyleBackColor = true;
+            this.btndepdisplay.Click += new System.EventHandler(this.btndepdisplay_Click);
+            // 
+            // comboBoxemployee
+            // 
+            this.comboBoxemployee.FormattingEnabled = true;
+            this.comboBoxemployee.Location = new System.Drawing.Point(103, 33);
+            this.comboBoxemployee.Name = "comboBoxemployee";
+            this.comboBoxemployee.Size = new System.Drawing.Size(278, 24);
+            this.comboBoxemployee.TabIndex = 10;
+            this.comboBoxemployee.SelectedIndexChanged += new System.EventHandler(this.comboBoxemployee_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(123, 71);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(278, 24);
+            this.comboBox2.TabIndex = 14;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 190);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1116, 659);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxemployee;
+        private System.Windows.Forms.Button btnempdisplay;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnempupdate;
+        private System.Windows.Forms.Button btnempadd;
+        private System.Windows.Forms.TextBox textBoxempname;
+        private System.Windows.Forms.TextBox textBoxid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btndepadd;
+        private System.Windows.Forms.Button btndepupdate;
+        private System.Windows.Forms.Button btndepdelete;
+        private System.Windows.Forms.Button btndepdisplay;
+        private System.Windows.Forms.TextBox textBoxdepid;
+        private System.Windows.Forms.TextBox textBoxdepartmentanme;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Entitylabdbfirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btndepdisplay_Click(object sender, EventArgs e)
+        {
+            EmploymentEntities1 epentitys = new EmploymentEntities1();
+
+
+            
+            
+                listBox1.Items.Clear();
+                comboBox2.Items.Clear();
+                var items = from i in epentitys.Departments select i;
+                foreach (var i in items)
+                {
+                    listBox1.Items.Add(i.id + i.deptName);
+                    comboBox2.Items.Add(i.id);
+
+                }
+                
+           
+           
+            
+        }
+
+        private void btnempdisplay_Click(object sender, EventArgs e)
+        {
+            EmploymentEntities1 epentitys = new EmploymentEntities1();
+
+
+
+
+            listBox1.Items.Clear();
+            comboBoxemployee.Items.Clear();
+            var items = from i in epentitys.Employees select i;
+            foreach (var i in items)
+            {
+                listBox1.Items.Add(i.id + "\t"+i.name + "\t" +i.Id_dept);
+                comboBoxemployee.Items.Add(i.id);
+               
+                
+
+            }
+        }
+
+        private void btndepadd_Click(object sender, EventArgs e)
+        {
+            EmploymentEntities1 ent = new EmploymentEntities1();
+            Department dep = new Department();
+            dep.id = int.Parse(textBoxdepid.Text);
+            dep.deptName = textBoxdepartmentanme.Text;
+            ent.Departments.Add(dep);
+            ent.SaveChanges();
+            textBoxdepid.Text = textBoxdepartmentanme.Text = " ";
+        }
+
+        private void btnempadd_Click(object sender, EventArgs e)
+        {
+            EmploymentEntities1 ent = new EmploymentEntities1();
+            Employee emp = new Employee();
+            emp.id = int.Parse(textBoxid.Text);
+           emp.name = textBoxempname.Text;
+            emp.Id_dept=int.Parse(textBox1.Text);
+            ent.Employees.Add(emp);
+            ent.SaveChanges();
+            textBox1.Text = textBoxempname.Text =textBoxid.Text= " ";
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EmploymentEntities1 ent = new EmploymentEntities1();
+            int ID = int.Parse(comboBox2.Text);
+            Department retrieve = ent.Departments.Find(ID);
+            if(ID != null)
+            {
+                textBoxdepid.Text = retrieve.id.ToString();
+                textBoxdepartmentanme.Text = retrieve.deptName;
+                listBox1.Items.Clear();
+                foreach(var d in retrieve.Employees)
+                {
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(d.name);
+                }
+            }
+        }
+
+        private void comboBoxemployee_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            EmploymentEntities1 ent = new EmploymentEntities1();
+            int ID = int.Parse(comboBoxemployee.Text);
+            Employee retrieve = ent.Employees.Find(ID);
+            if(ID != null)
+            {
+                textBoxid.Text = retrieve.id.ToString();
+                textBoxempname.Text = retrieve.name;
+                textBox1.Text = retrieve.Id_dept.ToString();
+                foreach(var i in retrieve.name)
+                {
+                    listBox1.Items.Clear();
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+
+        private void btndepupdate_Click(object sender, EventArgs e)
+        {
+            int ID = int.Parse(textBoxdepid.Text);
+            EmploymentEntities1 emp = new EmploymentEntities1();
+
+            var depart = emp.Departments.Where(d => d.id == ID).Select(d => d).First();
+            
+            depart.deptName = textBoxdepartmentanme.Text;
+            
+            emp.SaveChanges();
+        }
+
+        private void btndepdelete_Click(object sender, EventArgs e)
+        {
+
+            int ID = int.Parse(textBoxdepid.Text);
+            EmploymentEntities1 emp = new EmploymentEntities1();
+
+            var depart = emp.Departments.Where(d => d.id == ID).Select(d => d).First();
+            emp.Departments.Remove(depart);
+            emp.SaveChanges();
+        }
+
+        private void btnempupdate_Click(object sender, EventArgs e)
+        {
+            int ID = int.Parse(textBoxid.Text);
+            EmploymentEntities1 emp = new EmploymentEntities1();
+
+            var empls = emp.Employees.Where(d => d.id == ID).Select(d => d).First();
+            empls.name = textBoxempname.Text;
+            empls.Id_dept = int.Parse(textBox1.Text);
+            emp.SaveChanges();
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            int ID = int.Parse(textBoxid.Text);
+            EmploymentEntities1 emp = new EmploymentEntities1();
+
+            var empls = emp.Employees.Where(d => d.id == ID).Select(d => d).First();
+            emp.Employees.Remove(empls);
+            emp.SaveChanges();
+        }
+    }
+}
